2d4a6477354821e47403ac6f0bed5621e85e4753
Bemat <116222049+yuusuke1101@users.noreply.github.com>
2024-10-21 13:14:44 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,213 @@
+const kecamatanData = {
+    "01": [  // Kabupaten Pringsewu
+        { nama: "Pagelaran", kode: "01" },
+        { nama: "Adiluwih", kode: "02" },
+        { nama: "Ambarawa", kode: "03" },
+        { nama: "Banyumas", kode: "04" },
+        { nama: "Gading Rejo", kode: "05" },
+        { nama: "Pringsewu", kode: "06" },
+        { nama: "Sukoharjo", kode: "07" },
+        { nama: "Pagelaran Utara", kode: "08" }
+    ],
+    "07": [  // Kabupaten Tanggamus
+        { nama: "Blimbing", kode: "09" },
+        { nama: "Cukuh Balak", kode: "10" },
+        { nama: "Gisting", kode: "11" },
+        { nama: "Kota Agung", kode: "12" },
+        { nama: "Kota Agung Barat", kode: "13" },
+        { nama: "Kota Agung Timur", kode: "14" },
+        { nama: "Pugung", kode: "15" },
+        { nama: "Pulau Panggung", kode: "16" },
+        { nama: "Semaka", kode: "17" },
+        { nama: "Sumber Jaya", kode: "18" },
+        { nama: "Way Ratai", kode: "19" },
+        { nama: "Wonosobo", kode: "20" }
+    ],
+    "02": [  // Kabupaten Lampung Barat
+        { nama: "Balik Bukit", kode: "21" },
+        { nama: "Batu Brak", kode: "22" },
+        { nama: "Belalau", kode: "23" },
+        { nama: "Gedung Surian", kode: "24" },
+        { nama: "Kebun Tebu", kode: "25" },
+        { nama: "Lumbok Seminung", kode: "26" },
+        { nama: "Pagar Dewa", kode: "27" },
+        { nama: "Sekincau", kode: "28" },
+        { nama: "Sukau", kode: "29" },
+        { nama: "Sumber Jaya", kode: "30" },
+        { nama: "Suoh", kode: "31" },
+        { nama: "Way Tenong", kode: "32" }
+    ],
+    "03": [  // Kabupaten Lampung Selatan
+        { nama: "Bakauheni", kode: "33" },
+        { nama: "Candipuro", kode: "34" },
+        { nama: "Jati Agung", kode: "35" },
+        { nama: "Kalianda", kode: "36" },
+        { nama: "Katibung", kode: "37" },
+        { nama: "Ketapang", kode: "38" },
+        { nama: "Merbau Mataram", kode: "39" },
+        { nama: "Natar", kode: "40" },
+        { nama: "Palas", kode: "41" },
+        { nama: "Penengahan", kode: "42" },
+        { nama: "Rajabasa", kode: "43" },
+        { nama: "Sidomulyo", kode: "44" },
+        { nama: "Sragi", kode: "45" },
+        { nama: "Tanjung Bintang", kode: "46" },
+        { nama: "Tanjung Sari", kode: "47" },
+        { nama: "Way Panji", kode: "48" },
+        { nama: "Way Sulan", kode: "49" }
+    ],
+    "04": [  // Kabupaten Lampung Tengah
+        { nama: "Anak Ratu Aji", kode: "50" },
+        { nama: "Anak Tuha", kode: "51" },
+        { nama: "Bandar Mataram", kode: "52" },
+        { nama: "Bandar Surabaya", kode: "53" },
+        { nama: "Bangun Rejo", kode: "54" },
+        { nama: "Bekri", kode: "55" },
+        { nama: "Bumi Nabung", kode: "56" },
+        { nama: "Bumi Ratu Nuban", kode: "57" },
+        { nama: "Gunung Sugih", kode: "58" },
+        { nama: "Kalirejo", kode: "59" },
+        { nama: "Kota Gajah", kode: "60" },
+        { nama: "Padang Ratu", kode: "61" },
+        { nama: "Pubian", kode: "62" },
+        { nama: "Punggur", kode: "63" },
+        { nama: "Putra Rumbia", kode: "64" },
+        { nama: "Rumbia", kode: "65" },
+        { nama: "Selagai Lingga", kode: "66" },
+        { nama: "Sendang Agung", kode: "67" },
+        { nama: "Seputih Agung", kode: "68" },
+        { nama: "Seputih Banyak", kode: "69" },
+        { nama: "Seputih Mataram", kode: "70" },
+        { nama: "Seputih Raman", kode: "71" },
+        { nama: "Seputih Surabaya", kode: "72" },
+        { nama: "Terbanggi Besar", kode: "73" },
+        { nama: "Terusan Nunyai", kode: "74" },
+        { nama: "Trimurjo", kode: "75" }
+    ],
+    "05": [  // Kabupaten Lampung Timur
+        { nama: "Bandar Sribhawono", kode: "76" },
+        { nama: "Batanghari", kode: "77" },
+        { nama: "Batanghari Nuban", kode: "78" },
+        { nama: "Braja Slebah", kode: "79" },
+        { nama: "Labuhan Maringgai", kode: "80" },
+        { nama: "Marga Sekampung", kode: "81" },
+        { nama: "Mataram Baru", kode: "82" },
+        { nama: "Melinting", kode: "83" },
+        { nama: "Metro Kibang", kode: "84" },
+        { nama: "Pasir Sakti", kode: "85" },
+        { nama: "Pekalongan", kode: "86" },
+        { nama: "Purbolinggo", kode: "87" },
+        { nama: "Raman Utara", kode: "88" },
+        { nama: "Sekampung", kode: "89" },
+        { nama: "Sekampung Udik", kode: "90" },
+        { nama: "Sukadana", kode: "91" },
+        { nama: "Waway Karya", kode: "92" },
+        { nama: "Way Bungur", kode: "93" },
+        { nama: "Way Jepara", kode: "94" },
+        { nama: "Way Tuba", kode: "95" }
+    ],
+    "06": [  // Kabupaten Lampung Utara
+        { nama: "Abung Barat", kode: "96" },
+        { nama: "Abung Selatan", kode: "97" },
+        { nama: "Abung Semuli", kode: "98" },
+        { nama: "Abung Surakarta", kode: "99" },
+        { nama: "Bunga Mayang", kode: "100" },
+        { nama: "Kotabumi", kode: "101" },
+        { nama: "Kota Bumi", kode: "102" },
+        { nama: "Kota Gajah", kode: "103" },
+        { nama: "Napa", kode: "104" },
+        { nama: "Panjang", kode: "105" },
+        { nama: "Sukoharjo", kode: "106" },
+        { nama: "Tanjung Raja", kode: "107" }
+    ],
+    "08": [  // Kabupaten Pesawaran
+        { nama: "Gedong Tataan", kode: "108" },
+        { nama: "Kedondong", kode: "109" },
+        { nama: "Marga Punduh", kode: "110" },
+        { nama: "Pesawaran", kode: "111" },
+        { nama: "Way Lima", kode: "112" }
+    ],
+    "09": [  // Kabupaten Way Kanan
+        { nama: "Baradatu", kode: "113" },
+        { nama: "Banjit", kode: "114" },
+        { nama: "Buay Bahuga", kode: "115" },
+        { nama: "Buay Pematang Rwa", kode: "116" },
+        { nama: "Kasui", kode: "117" },
+        { nama: "Negeri Agung", kode: "118" },
+        { nama: "Panjang Jaya", kode: "119" },
+        { nama: "Way Tuba", kode: "120" },
+        { nama: "Way Kanan", kode: "121" },
+        { nama: "Way Serdang", kode: "122" }
+    ],
+    "10": [  // Kabupaten Mesuji
+        { nama: "Mesuji", kode: "123" },
+        { nama: "Panca Jaya", kode: "124" },
+        { nama: "Tanjung Raya", kode: "125" }
+    ],
+    "11": [  // Kabupaten Tulang Bawang
+        { nama: "Banjar Agung", kode: "126" },
+        { nama: "Menggala", kode: "127" },
+        { nama: "Mesarit", kode: "128" },
+        { nama: "Penawar Tama", kode: "129" },
+        { nama: "Tulang Bawang", kode: "130" },
+        { nama: "Tulang Bawang Barat", kode: "131" }
+    ],
+    "12": [  // Kabupaten Tulang Bawang Barat
+        { nama: "Gunung Terang", kode: "132" },
+        { nama: "Tulang Bawang", kode: "133" },
+        { nama: "Tulang Bawang Barat", kode: "134" }
+    ],
+    "13": [  // Kota Bandar Lampung
+        { nama: "Kedaton", kode: "135" },
+        { nama: "Langkapura", kode: "136" },
+        { nama: "Rajabasa", kode: "137" },
+        { nama: "Tanjung Karang Pusat", kode: "138" },
+        { nama: "Tanjung Karang Timur", kode: "139" },
+        { nama: "Tanjung Karang Barat", kode: "140" },
+        { nama: "Kota Baru", kode: "141" },
+        { nama: "Bumiratu Nuban", kode: "142" }
+    ],
+    "14": [  // Kota Metro
+        { nama: "Metro Barat", kode: "143" },
+        { nama: "Metro Center", kode: "144" },
+        { nama: "Metro Timur", kode: "145" },
+        { nama: "Metro Selatan", kode: "146" },
+        { nama: "Metro Utara", kode: "147" }
+    ]
+};
+
+function updateKecamatan() {
+    const kabupatenSelect = document.getElementById('kabupaten');
+    const kecamatanSelect = document.getElementById('kecamatan');
+    const kodeWilayahDiv = document.getElementById('kode-wilayah');
+
+    // Kosongkan dropdown kecamatan
+    kecamatanSelect.innerHTML = '<option value="">Pilih Kecamatan</option>';
+    kodeWilayahDiv.innerHTML = ''; // Kosongkan div kode wilayah
+
+    const selectedKabupaten = kabupatenSelect.value;
+
+    if (selectedKabupaten) {
+        const kecamatans = kecamatanData[selectedKabupaten];
+        kecamatans.forEach(kecamatan => {
+            const option = document.createElement('option');
+            option.value = kecamatan.kode;
+            option.textContent = kecamatan.nama;
+            kecamatanSelect.appendChild(option);
+        });
+    }
+}
+
+const kecamatanSelect = document.getElementById('kecamatan');
+
+kecamatanSelect.onchange = function () {
+    const selectedKecamatan = kecamatanSelect.options[kecamatanSelect.selectedIndex];
+    const kodeWilayah = selectedKecamatan.value;
+    const kecamatanName = selectedKecamatan.textContent;
+
+    if (kodeWilayah) {
+        document.getElementById('kode-wilayah').innerHTML = `Kode Wilayah: ${kodeWilayah} - Kecamatan: ${kecamatanName}`;
+    } else {
+        document.getElementById('kode-wilayah').innerHTML = '';
+    }
+};
