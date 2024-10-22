29a588a4eeeac1d68d93029fb84b78f1653c6f72
Bemat <116222049+yuusuke1101@users.noreply.github.com>
2024-10-22 10:38:23 +0700

    Add files via upload

---
+++ sumsel/script.js
@@ -0,0 +1,313 @@
+const kecamatanData = {
+    "01": [  // Kabupaten Banyuasin
+        { nama: "Air Kumbang", kode: "D001" },
+        { nama: "Air Salek", kode: "D002" },
+        { nama: "Banyuasin I", kode: "D003" },
+        { nama: "Banyuasin II", kode: "D004" },
+        { nama: "Banyuasin III", kode: "D005" },
+        { nama: "Betung", kode: "D006" },
+        { nama: "Makarti Jaya", kode: "D007" },
+        { nama: "Muara Padang", kode: "D008" },
+        { nama: "Muara Sugihan", kode: "D009" },
+        { nama: "Pulau Rimau", kode: "D010" },
+        { nama: "Rambutan", kode: "D011" },
+        { nama: "Rantau Bayur", kode: "D012" },
+        { nama: "Sembawa", kode: "D013" },
+        { nama: "Selat Penuguan", kode: "D014" },
+        { nama: "Suak Tapeh", kode: "D015" },
+        { nama: "Talang Kelapa", kode: "D016" },
+        { nama: "Tanjung Lago", kode: "D017" },
+        { nama: "Tungkal Ilir", kode: "D018" },
+        { nama: "Tungkal Jaya", kode: "D019" },
+        { nama: "Karang Agung Ilir", kode: "D020" },
+        { nama: "Banyuasin Selatan", kode: "D021" }
+    ],
+    "02": [  // Kabupaten Empat Lawang
+        { nama: "Lahat", kode: "D022" },
+        { nama: "Pendopo", kode: "D023" },
+        { nama: "Pendopo Barat", kode: "D024" },
+        { nama: "Muara Pinang", kode: "D025" },
+        { nama: "Pasemah Air Keruh", kode: "D026" },
+        { nama: "Talang Padang", kode: "D027" },
+        { nama: "Sikap Dalam", kode: "D028" },
+        { nama: "Tebing Tinggi", kode: "D029" },
+        { nama: "Saling", kode: "D030" },
+        { nama: "Ulu Musi", kode: "D031" }
+    ],
+    "03": [  // Kabupaten Lahat
+        { nama: "Gumay Talang", kode: "D032" },
+        { nama: "Gumay Ulu", kode: "D033" },
+        { nama: "Jarai", kode: "D034" },
+        { nama: "Kikim Barat", kode: "D035" },
+        { nama: "Kikim Selatan", kode: "D036" },
+        { nama: "Kikim Tengah", kode: "D037" },
+        { nama: "Kikim Timur", kode: "D038" },
+        { nama: "Kota Agung", kode: "D039" },
+        { nama: "Lahat", kode: "D040" },
+        { nama: "Merapi Barat", kode: "D041" },
+        { nama: "Merapi Selatan", kode: "D042" },
+        { nama: "Merapi Timur", kode: "D043" },
+        { nama: "Mulak Sebingkai", kode: "D044" },
+        { nama: "Mulak Ulu", kode: "D045" },
+        { nama: "Pagar Gunung", kode: "D046" },
+        { nama: "Pajar Bulan", kode: "D047" },
+        { nama: "Pulau Pinang", kode: "D048" },
+        { nama: "Suka Merindu", kode: "D049" },
+        { nama: "Tanjung Sakti Pumi", kode: "D050" },
+        { nama: "Tanjung Sakti Pumu", kode: "D051" },
+        { nama: "Tanjung Tebat", kode: "D052" },
+        { nama: "Pseksu", kode: "D053" },
+        { nama: "Sukamerindu", kode: "D054" },
+        { nama: "Lintang Kanan", kode: "D055" }
+    ],
+    "04": [  // Kabupaten Muara Enim
+        { nama: "Belimbing", kode: "D056" },
+        { nama: "Benakat", kode: "D057" },
+        { nama: "Gelumbang", kode: "D058" },
+        { nama: "Gunung Megang", kode: "D059" },
+        { nama: "Kelekar", kode: "D060" },
+        { nama: "Lawang Kidul", kode: "D061" },
+        { nama: "Lembak", kode: "D062" },
+        { nama: "Lubai", kode: "D063" },
+        { nama: "Lubai Ulu", kode: "D064" },
+        { nama: "Muara Enim", kode: "D065" },
+        { nama: "Panang Enim", kode: "D066" },
+        { nama: "Rambang", kode: "D067" },
+        { nama: "Rambang Dangku", kode: "D068" },
+        { nama: "Semende Darat Laut", kode: "D069" },
+        { nama: "Semende Darat Tengah", kode: "D070" },
+        { nama: "Semende Darat Ulu", kode: "D071" },
+        { nama: "Sungai Rotan", kode: "D072" },
+        { nama: "Tanjung Agung", kode: "D073" },
+        { nama: "Tanjung Enim", kode: "D074" },
+        { nama: "Ujan Mas", kode: "D075" },
+        { nama: "Empat Petulai Dangku", kode: "D076" },
+        { nama: "Muara Belida", kode: "D077" }
+    ],
+    "05": [  // Kabupaten Musi Banyuasin
+        { nama: "Babat Supat", kode: "D078" },
+        { nama: "Babat Toman", kode: "D079" },
+        { nama: "Batanghari Leko", kode: "D080" },
+        { nama: "Bayung Lencir", kode: "D081" },
+        { nama: "Keluang", kode: "D082" },
+        { nama: "Lais", kode: "D083" },
+        { nama: "Lalan", kode: "D084" },
+        { nama: "Lawang Wetan", kode: "D085" },
+        { nama: "Plakat Tinggi", kode: "D086" },
+        { nama: "Sanga Desa", kode: "D087" },
+        { nama: "Sekayu", kode: "D088" },
+        { nama: "Sungai Keruh", kode: "D089" },
+        { nama: "Sungai Lilin", kode: "D090" },
+        { nama: "Tungkal Jaya", kode: "D091" },
+        { nama: "Jirak Jaya", kode: "D092" }
+    ],
+    "06": [  // Kabupaten Musi Rawas
+        { nama: "Jayaloka", kode: "D093" },
+        { nama: "Karang Dapo", kode: "D094" },
+        { nama: "Karang Jaya", kode: "D095" },
+        { nama: "Lubuklinggau", kode: "D096" },
+        { nama: "Megang Sakti", kode: "D097" },
+        { nama: "Muara Beliti", kode: "D098" },
+        { nama: "Muara Kelingi", kode: "D099" },
+        { nama: "Muara Lakitan", kode: "D100" },
+        { nama: "Purwodadi", kode: "D101" },
+        { nama: "Rawas Ilir", kode: "D102" },
+        { nama: "Selangit", kode: "D103" },
+        { nama: "Sukakarya", kode: "D104" },
+        { nama: "STL Ulu Terawas", kode: "D105" },
+        { nama: "Tugumulyo", kode: "D106" }
+    ],
+    "07": [  // Kabupaten Musi Rawas Utara
+        { nama: "Karang Dapo", kode: "D107" },
+        { nama: "Karang Jaya", kode: "D108" },
+        { nama: "Muara Rupit", kode: "D109" },
+        { nama: "Nibung", kode: "D110" },
+        { nama: "Rawas Ilir", kode: "D111" },
+        { nama: "Rawas Ulu", kode: "D112" },
+        { nama: "Ulu Rawas", kode: "D113" }
+    ],
+    "08": [  // Kabupaten Ogan Ilir
+        { nama: "Indralaya", kode: "D114" },
+        { nama: "Indralaya Selatan", kode: "D115" },
+        { nama: "Indralaya Utara", kode: "D116" },
+        { nama: "Lubuk Keliat", kode: "D117" },
+        { nama: "Muara Kuang", kode: "D118" },
+        { nama: "Payaraman", kode: "D119" },
+        { nama: "Pemulutan", kode: "D120" },
+        { nama: "Pemulutan Barat", kode: "D121" },
+        { nama: "Pemulutan Selatan", kode: "D122" },
+        { nama: "Rantau Alai", kode: "D123" },
+        { nama: "Rambang Kuang", kode: "D124" },
+        { nama: "Sungai Pinang", kode: "D125" },
+        { nama: "Tanjung Batu", kode: "D126" },
+        { nama: "Tanjung Raja", kode: "D127" },
+        { nama: "Kandis", kode: "D128" },
+        { nama: "Rantau Panjang", kode: "D129" }
+    ],
+    "09": [  // Kabupaten Ogan Komering Ilir
+        { nama: "Air Sugihan", kode: "D130" },
+        { nama: "Cengal", kode: "D131" },
+        { nama: "Jejawi", kode: "D132" },
+        { nama: "Kayu Agung", kode: "D133" },
+        { nama: "Kota Kayu Agung", kode: "D134" },
+        { nama: "Lempuing", kode: "D135" },
+        { nama: "Lempuing Jaya", kode: "D136" },
+        { nama: "Mesuji", kode: "D137" },
+        { nama: "Mesuji Makmur", kode: "D138" },
+        { nama: "Mesuji Raya", kode: "D139" },
+        { nama: "Pampangan", kode: "D140" },
+        { nama: "Pedamaran", kode: "D141" },
+        { nama: "Pedamaran Timur", kode: "D142" },
+        { nama: "Pematang Panggang", kode: "D143" },
+        { nama: "Sirah Pulau Padang", kode: "D144" },
+        { nama: "Sungai Menang", kode: "D145" },
+        { nama: "Teluk Gelam", kode: "D146" },
+        { nama: "Tulung Selapan", kode: "D147" }
+    ],
+    "10": [  // Kabupaten Ogan Komering Ulu
+        { nama: "Baturaja Barat", kode: "D148" },
+        { nama: "Baturaja Timur", kode: "D149" },
+        { nama: "Lubuk Batang", kode: "D150" },
+        { nama: "Lengkiti", kode: "D151" },
+        { nama: "Lubuk Raja", kode: "D152" },
+        { nama: "Muara Jaya", kode: "D153" },
+        { nama: "Pengandonan", kode: "D154" },
+        { nama: "Peninjauan", kode: "D155" },
+        { nama: "Semidang Aji", kode: "D156" },
+        { nama: "Sosoh Buay Rayap", kode: "D157" },
+        { nama: "Sinar Peninjauan", kode: "D158" },
+        { nama: "Ulu Ogan", kode: "D159" },
+        { nama: "Kedaton Peninjauan Raya", kode: "D160" }
+    ],
+    "11": [  // Kabupaten Ogan Komering Ulu Selatan
+        { nama: "Banding Agung", kode: "D161" },
+        { nama: "Buay Pemaca", kode: "D162" },
+        { nama: "Buay Rawan", kode: "D163" },
+        { nama: "Buay Sandang Aji", kode: "D164" },
+        { nama: "Buana Pemaca", kode: "D165" },
+        { nama: "Kisam Ilir", kode: "D166" },
+        { nama: "Kisam Tinggi", kode: "D167" },
+        { nama: "Mekakau Ilir", kode: "D168" },
+        { nama: "Muaradua", kode: "D169" },
+        { nama: "Muaradua Kisam", kode: "D170" },
+        { nama: "Pulau Beringin", kode: "D171" },
+        { nama: "Runjung Agung", kode: "D172" },
+        { nama: "Simpang", kode: "D173" },
+        { nama: "Sindang Danau", kode: "D174" },
+        { nama: "Sungai Are", kode: "D175" },
+        { nama: "Tiga Dihaji", kode: "D176" },
+        { nama: "Warkuk Ranau Selatan", kode: "D177" },
+        { nama: "Mekakau Ulu", kode: "D178" },
+        { nama: "Buay Runjung", kode: "D179" }
+    ],
+    "12": [  // Kabupaten Ogan Komering Ulu Timur
+        { nama: "Belitang", kode: "D180" },
+        { nama: "Belitang II", kode: "D181" },
+        { nama: "Belitang III", kode: "D182" },
+        { nama: "Belitang Jaya", kode: "D183" },
+        { nama: "Belitang Madang Raya", kode: "D184" },
+        { nama: "Belitang Mulya", kode: "D185" },
+        { nama: "Buay Madang", kode: "D186" },
+        { nama: "Buay Madang Timur", kode: "D187" },
+        { nama: "Buay Pemuka Bangsa Raja", kode: "D188" },
+        { nama: "Buay Pemuka Peliung", kode: "D189" },
+        { nama: "Cempaka", kode: "D190" },
+        { nama: "Jayapura", kode: "D191" },
+        { nama: "Madang Suku I", kode: "D192" },
+        { nama: "Madang Suku II", kode: "D193" },
+        { nama: "Martapura", kode: "D194" },
+        { nama: "Semendawai Barat", kode: "D195" },
+        { nama: "Semendawai Suku III", kode: "D196" },
+        { nama: "Semendawai Timur", kode: "D197" },
+        { nama: "Tugumulyo", kode: "D198" },
+        { nama: "Peninjauan", kode: "D199" }
+    ],
+    "13": [  // Kabupaten Penukal Abab Lematang Ilir
+        { nama: "Abab", kode: "D200" },
+        { nama: "Penukal", kode: "D201" },
+        { nama: "Penukal Utara", kode: "D202" },
+        { nama: "Talang Ubi", kode: "D203" },
+        { nama: "Tanah Abang", kode: "D204" }
+    ],
+    "14": [  // Kota Lubuklinggau
+        { nama: "Lubuklinggau Barat I", kode: "D205" },
+        { nama: "Lubuklinggau Barat II", kode: "D206" },
+        { nama: "Lubuklinggau Selatan I", kode: "D207" },
+        { nama: "Lubuklinggau Selatan II", kode: "D208" },
+        { nama: "Lubuklinggau Timur I", kode: "D209" },
+        { nama: "Lubuklinggau Timur II", kode: "D210" },
+        { nama: "Lubuklinggau Utara I", kode: "D211" },
+        { nama: "Lubuklinggau Utara II", kode: "D212" }
+    ],
+    "15": [  // Kota Pagar Alam
+        { nama: "Dempo Selatan", kode: "D213" },
+        { nama: "Dempo Tengah", kode: "D214" },
+        { nama: "Dempo Utara", kode: "D215" },
+        { nama: "Pagar Alam Selatan", kode: "D216" },
+        { nama: "Pagar Alam Utara", kode: "D217" }
+    ],
+    "16": [  // Kota Palembang
+        { nama: "Alang-Alang Lebar", kode: "D218" },
+        { nama: "Bukit Kecil", kode: "D219" },
+        { nama: "Gandus", kode: "D220" },
+        { nama: "Ilir Barat I", kode: "D221" },
+        { nama: "Ilir Barat II", kode: "D222" },
+        { nama: "Ilir Timur I", kode: "D223" },
+        { nama: "Ilir Timur II", kode: "D224" },
+        { nama: "Kalidoni", kode: "D225" },
+        { nama: "Kemuning", kode: "D226" },
+        { nama: "Kertapati", kode: "D227" },
+        { nama: "Plaju", kode: "D228" },
+        { nama: "Sako", kode: "D229" },
+        { nama: "Seberang Ulu I", kode: "D230" },
+        { nama: "Seberang Ulu II", kode: "D231" },
+        { nama: "Sematang Borang", kode: "D232" },
+        { nama: "Sukarami", kode: "D233" },
+        { nama: "Jakabaring", kode: "D234" },
+        { nama: "Talang Kelapa", kode: "D235" }
+    ],
+    "17": [  // Kota Prabumulih
+        { nama: "Cambai", kode: "D236" },
+        { nama: "Prabumulih Barat", kode: "D237" },
+        { nama: "Prabumulih Selatan", kode: "D238" },
+        { nama: "Prabumulih Timur", kode: "D239" },
+        { nama: "Prabumulih Utara", kode: "D240" },
+        { nama: "Rambang Kapak Tengah", kode: "D241" }
+    ]
+};
+
+
+// Update functions remain unchanged
+function updateKecamatan() {
+    const kabupatenSelect = document.getElementById('kabupaten');
+    const kecamatanSelect = document.getElementById('kecamatan');
+    
+    kecamatanSelect.innerHTML = '<option value="">Pilih Kecamatan</option>';
+    
+    const selectedKabupaten = kabupatenSelect.value;
+    const kecamatans = kecamatanData[selectedKabupaten];
+    
+    if (kecamatans) {
+        kecamatans.forEach(kecamatan => {
+            const option = document.createElement('option');
+            option.value = kecamatan.kode;
+            option.textContent = kecamatan.nama;
+            kecamatanSelect.appendChild(option);
+        });
+    }
+}
+
+function updateKodeWilayah() {
+    const kecamatanSelect = document.getElementById('kecamatan');
+    
+    const selectedKecamatan = kecamatanSelect.value;
+    
+    if (selectedKecamatan) {
+        const kodeWilayah = selectedKecamatan;
+        document.getElementById('kode-wilayah').innerText = kodeWilayah;
+        document.getElementById('contoh-output').innerText = `{${kodeWilayah}RAHMAD}`;
+        document.getElementById('output').style.display = 'block';
+    } else {
+        document.getElementById('output').style.display = 'none';
+    }
+}
