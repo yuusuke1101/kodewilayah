b4f64dbf0be3d6fef7f600bd7253bbc436d8b966
Bemat <116222049+yuusuke1101@users.noreply.github.com>
2024-10-22 09:50:15 +0700

    Add files via upload

---
+++ jambi/script.js
@@ -0,0 +1,200 @@
+const kecamatanData = {
+    "01": [  // Kabupaten Batanghari
+        { nama: "Bajubang", kode: "C01" },
+        { nama: "Bathin XXIV", kode: "C02" },
+        { nama: "Maro Sebo Ilir", kode: "C03" },
+        { nama: "Maro Sebo Ulu", kode: "C04" },
+        { nama: "Muara Bulian", kode: "C05" },
+        { nama: "Mersam", kode: "C06" },
+        { nama: "Pemayung", kode: "C07" },
+        { nama: "Muara Tembesi", kode: "C08" }
+    ],
+    "02": [  // Kabupaten Bungo
+        { nama: "Batin II Babeko", kode: "C09" },
+        { nama: "Bathin III", kode: "C10" },
+        { nama: "Bathin III Ulu", kode: "C11" },
+        { nama: "Jujuhan", kode: "C12" },
+        { nama: "Jujuhan Ilir", kode: "C13" },
+        { nama: "Limbur Lubuk Mengkuang", kode: "C14" },
+        { nama: "Muko-Muko Bathin VII", kode: "C15" },
+        { nama: "Pasar Muara Bungo", kode: "C16" },
+        { nama: "Pelepat", kode: "C17" },
+        { nama: "Pelepat Ilir", kode: "C18" },
+        { nama: "Rantau Pandan", kode: "C19" },
+        { nama: "Rimbo Tengah", kode: "C20" },
+        { nama: "Tanah Sepenggal", kode: "C21" },
+        { nama: "Tanah Sepenggal Lintas", kode: "C22" },
+        { nama: "Tanah Tumbuh", kode: "C23" },
+        { nama: "Bathin II Pelayang", kode: "C24" } ,
+		{ nama: "Bungo Dani", kode: "C141" }
+    ],
+    "03": [  // Kabupaten Kerinci
+        { nama: "Air Hangat", kode: "C25" },
+        { nama: "Air Hangat Barat", kode: "C26" },
+        { nama: "Air Hangat Timur", kode: "C27" },
+        { nama: "Batang Merangin", kode: "C28" },
+        { nama: "Bukit Kerman", kode: "C29" },
+        { nama: "Danau Kerinci", kode: "C30" },
+        { nama: "Depati Tujuh", kode: "C31" },
+        { nama: "Gunung Kerinci", kode: "C32" },
+        { nama: "Gunung Raya", kode: "C33" },
+        { nama: "Kayu Aro", kode: "C34" },
+        { nama: "Kayu Aro Barat", kode: "C35" },
+        { nama: "Keliling Danau", kode: "C36" },
+        { nama: "Sitinjau Laut", kode: "C37" },
+        { nama: "Siulak", kode: "C38" },
+        { nama: "Siulak Mukai", kode: "C39" },
+        { nama: "Gunung Tujuh", kode: "C40" }
+    ],
+    "04": [  // Kabupaten Merangin
+        { nama: "Bangko", kode: "C41" },
+        { nama: "Bangko Barat", kode: "C42" },
+        { nama: "Batang Masumai", kode: "C43" },
+        { nama: "Jangkat", kode: "C44" },
+        { nama: "Jangkat Timur", kode: "C45" },
+        { nama: "Lembah Masurai", kode: "C46" },
+        { nama: "Muara Siau", kode: "C47" },
+        { nama: "Nalo Tantan", kode: "C48" },
+        { nama: "Pamenang", kode: "C49" },
+        { nama: "Pamenang Barat", kode: "C50" },
+        { nama: "Pamenang Selatan", kode: "C51" },
+        { nama: "Pangkalan Jambu", kode: "C52" },
+        { nama: "Renah Pembarap", kode: "C53" },
+        { nama: "Renah Pemenang", kode: "C54" },
+        { nama: "Sungai Manau", kode: "C55" },
+        { nama: "Tabir", kode: "C56" },
+        { nama: "Tabir Barat", kode: "C57" },
+        { nama: "Tabir Ilir", kode: "C58" },
+        { nama: "Tabir Lintas", kode: "C59" },
+        { nama: "Tabir Selatan", kode: "C60" },
+        { nama: "Tabir Timur", kode: "C61" },
+        { nama: "Tabir Ulu", kode: "C62" },
+        { nama: "Tiang Pumpung", kode: "C63" },
+        { nama: "Tabir Utara", kode: "C64" }
+    ],
+    "05": [  // Kabupaten Muaro Jambi
+        { nama: "Bahar Selatan", kode: "C65" },
+        { nama: "Bahar Utara", kode: "C66" },
+        { nama: "Jambi Luar Kota", kode: "C67" },
+        { nama: "Kumpeh", kode: "C68" },
+        { nama: "Kumpeh Ulu", kode: "C69" },
+        { nama: "Maro Sebo", kode: "C70" },
+        { nama: "Mestong", kode: "C71" },
+        { nama: "Sekernan", kode: "C72" },
+        { nama: "Sungai Bahar", kode: "C73" },
+        { nama: "Sungai Gelam", kode: "C74" },
+        { nama: "Taman Rajo", kode: "C75" }
+    ],
+    "06": [  // Kabupaten Sarolangun
+        { nama: "Air Hitam", kode: "C76" },
+        { nama: "Bathin VIII", kode: "C77" },
+        { nama: "Cermin Nan Gedang", kode: "C78" },
+        { nama: "Limun", kode: "C79" },
+        { nama: "Mandiangin", kode: "C80" },
+        { nama: "Pauh", kode: "C81" },
+        { nama: "Pelawan", kode: "C82" },
+        { nama: "Sarolangun", kode: "C83" },
+        { nama: "Singkut", kode: "C84" },
+        { nama: "Batang Asai", kode: "C85" }
+    ],
+    "07": [  // Kabupaten Tanjung Jabung Barat
+        { nama: "Batang Asam", kode: "C86" },
+        { nama: "Bram Itam", kode: "C87" },
+        { nama: "Kuala Betara", kode: "C88" },
+        { nama: "Merlung", kode: "C89" },
+        { nama: "Muara Papalik", kode: "C90" },
+        { nama: "Pengabuan", kode: "C91" },
+        { nama: "Renah Mendaluh", kode: "C92" },
+        { nama: "Seberang Kota", kode: "C93" },
+        { nama: "Senyerang", kode: "C94" },
+        { nama: "Tebing Tinggi", kode: "C95" },
+        { nama: "Tungkal Ilir", kode: "C96" },
+        { nama: "Tungkal Ulu", kode: "C97" },
+        { nama: "Betara", kode: "C98" }
+    ],
+    "08": [  // Kabupaten Tanjung Jabung Timur
+        { nama: "Berbak", kode: "C99" },
+        { nama: "Dendang", kode: "C100" },
+        { nama: "Geragai", kode: "C101" },
+        { nama: "Kuala Jambi", kode: "C102" },
+        { nama: "Mendahara", kode: "C103" },
+        { nama: "Mendahara Ulu", kode: "C104" },
+        { nama: "Muara Sabak Barat", kode: "C105" },
+        { nama: "Muara Sabak Timur", kode: "C106" },
+        { nama: "Nipah Panjang", kode: "C107" },
+        { nama: "Rantau Rasau", kode: "C108" },
+        { nama: "Sadu", kode: "C109" }
+    ],
+    "09": [  // Kabupaten Tebo
+        { nama: "Muara Tabir", kode: "C110" },
+        { nama: "Rimbo Bujang", kode: "C111" },
+        { nama: "Rimbo Ilir", kode: "C112" },
+        { nama: "Rimbo Ulu", kode: "C113" },
+        { nama: "Serai Serumpun", kode: "C114" },
+        { nama: "Sumay", kode: "C115" },
+        { nama: "Tebo Ilir", kode: "C116" },
+        { nama: "Tebo Tengah", kode: "C117" },
+        { nama: "Tebo Ulu", kode: "C118" },
+        { nama: "Tengah Ilir", kode: "C119" },
+        { nama: "VII Koto", kode: "C120" },
+        { nama: "VII Koto Ilir", kode: "C121" }
+    ],
+    "10": [  // Kota Jambi
+        { nama: "Alam Barajo", kode: "C122" },
+        { nama: "Danau Sipin", kode: "C123" },
+        { nama: "Danau Teluk", kode: "C124" },
+        { nama: "Jambi Selatan", kode: "C125" },
+        { nama: "Jambi Timur", kode: "C126" },
+        { nama: "Jelutung", kode: "C127" },
+        { nama: "Kota Baru", kode: "C128" },
+        { nama: "Pasar Jambi", kode: "C129" },
+        { nama: "Pelayangan", kode: "C130" },
+        { nama: "Telanaipura", kode: "C131" },
+        { nama: "Paal Merah", kode: "C132" }
+    ],
+    "11": [  // Kota Sungai Penuh
+        { nama: "Hamparan Rawang", kode: "C133" },
+        { nama: "Koto Baru", kode: "C134" },
+        { nama: "Kumun Debai", kode: "C135" },
+        { nama: "Pesisir Bukit", kode: "C136" },
+        { nama: "Pondok Tinggi", kode: "C137" },
+        { nama: "Sungai Bungkal", kode: "C138" },
+        { nama: "Sungai Penuh", kode: "C139" },
+        { nama: "Tanah Kampung", kode: "C140" }
+    ]
+};
+
+// Update functions remain unchanged
+function updateKecamatan() {
+    const kabupatenSelect = document.getElementById('kabupaten');
+    const kecamatanSelect = document.getElementById('kecamatan');
+    
+    kecamatanSelect.innerHTML = '<option value="">Pilih Kecamatan</option>';
+    
+    const selectedKabupaten = kabupatenSelect.value;
+    const kecamatans = kecamatanData[selectedKabupaten];
+    
+    if (kecamatans) {
+        kecamatans.forEach(kecamatan => {
+            const option = document.createElement('option');
+            option.value = kecamatan.kode;
+            option.textContent = kecamatan.nama;
+            kecamatanSelect.appendChild(option);
+        });
+    }
+}
+
+function updateKodeWilayah() {
+    const kecamatanSelect = document.getElementById('kecamatan');
+    
+    const selectedKecamatan = kecamatanSelect.value;
+    
+    if (selectedKecamatan) {
+        const kodeWilayah = selectedKecamatan;
+        document.getElementById('kode-wilayah').innerText = kodeWilayah;
+        document.getElementById('contoh-output').innerText = `{${kodeWilayah}RAHMAD}`;
+        document.getElementById('output').style.display = 'block';
+    } else {
+        document.getElementById('output').style.display = 'none';
+    }
+}
