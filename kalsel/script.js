d1b3d6eee50196b6eb808bb507c9815219dca8e4
Bemat <116222049+yuusuke1101@users.noreply.github.com>
2024-10-22 14:13:12 +0700

    Add files via upload

---
+++ kalsel/script.js
@@ -0,0 +1,215 @@
+const kecamatanData = {
+    "01": [  // Kabupaten Balangan
+        { nama: "Awayan", kode: "F01" },
+        { nama: "Batu Mandi", kode: "F02" },
+        { nama: "Halong", kode: "F03" },
+        { nama: "Juai", kode: "F04" },
+        { nama: "Lampihong", kode: "F05" },
+        { nama: "Paringin", kode: "F06" },
+        { nama: "Paringin Selatan", kode: "F07" },
+        { nama: "Tebing Tinggi", kode: "F08" }
+    ],
+    "02": [  // Kabupaten Banjar
+        { nama: "Aluh-Aluh", kode: "F09" },
+        { nama: "Aranio", kode: "F10" },
+        { nama: "Astambul", kode: "F11" },
+        { nama: "Beruntung Baru", kode: "F12" },
+        { nama: "Gambut", kode: "F13" },
+        { nama: "Karang Intan", kode: "F14" },
+        { nama: "Kertak Hanyar", kode: "F15" },
+        { nama: "Mataraman", kode: "F16" },
+        { nama: "Martapura", kode: "F17" },
+        { nama: "Martapura Barat", kode: "F18" },
+        { nama: "Martapura Timur", kode: "F19" },
+        { nama: "Pengaron", kode: "F20" },
+        { nama: "Sambung Makmur", kode: "F21" },
+        { nama: "Simpang Empat", kode: "F22" },
+        { nama: "Sungai Pinang", kode: "F23" },
+        { nama: "Sungai Tabuk", kode: "F24" },
+        { nama: "Tatah Makmur", kode: "F25" },
+        { nama: "Telaga Bauntung", kode: "F26" },
+        { nama: "Paramasan", kode: "F27" },
+        { nama: "Sambung Makmur", kode: "F28" } // Data sambung makmur diulang
+    ],
+    "03": [  // Kabupaten Barito Kuala
+        { nama: "Alalak", kode: "F29" },
+        { nama: "Anjir Muara", kode: "F30" },
+        { nama: "Anjir Pasar", kode: "F31" },
+        { nama: "Bakumpai", kode: "F32" },
+        { nama: "Barambai", kode: "F33" },
+        { nama: "Belawang", kode: "F34" },
+        { nama: "Cerbon", kode: "F35" },
+        { nama: "Jejangkit", kode: "F36" },
+        { nama: "Kuripan", kode: "F37" },
+        { nama: "Mandastana", kode: "F38" },
+        { nama: "Marabahan", kode: "F39" },
+        { nama: "Mekarsari", kode: "F40" },
+        { nama: "Rantau Badauh", kode: "F41" },
+        { nama: "Tabunganen", kode: "F42" },
+        { nama: "Tamban", kode: "F43" },
+        { nama: "Wanaraya", kode: "F44" },
+        { nama: "Tabunganen", kode: "F45" } // Data tabunganen diulang
+    ],
+    "04": [  // Kabupaten Hulu Sungai Selatan
+        { nama: "Angkinang", kode: "F46" },
+        { nama: "Daha Barat", kode: "F47" },
+        { nama: "Daha Selatan", kode: "F48" },
+        { nama: "Daha Utara", kode: "F49" },
+        { nama: "Kalumpang", kode: "F50" },
+        { nama: "Kandangan", kode: "F51" },
+        { nama: "Loksado", kode: "F52" },
+        { nama: "Padang Batung", kode: "F53" },
+        { nama: "Simpur", kode: "F54" },
+        { nama: "Sungai Raya", kode: "F55" },
+        { nama: "Telaga Langsat", kode: "F56" }
+    ],
+    "05": [  // Kabupaten Hulu Sungai Tengah
+        { nama: "Barabai", kode: "F57" },
+        { nama: "Batu Benawa", kode: "F58" },
+        { nama: "Batang Alai Selatan", kode: "F59" },
+        { nama: "Batang Alai Timur", kode: "F60" },
+        { nama: "Batang Alai Utara", kode: "F61" },
+        { nama: "Haruyan", kode: "F62" },
+        { nama: "Hantakan", kode: "F63" },
+        { nama: "Labuan Amas Selatan", kode: "F64" },
+        { nama: "Labuan Amas Utara", kode: "F65" },
+        { nama: "Limpasu", kode: "F66" },
+        { nama: "Pandawan", kode: "F67" }
+    ],
+    "06": [  // Kabupaten Hulu Sungai Utara
+        { nama: "Amuntai Selatan", kode: "F68" },
+        { nama: "Amuntai Tengah", kode: "F69" },
+        { nama: "Amuntai Utara", kode: "F70" },
+        { nama: "Babirik", kode: "F71" },
+        { nama: "Banjang", kode: "F72" },
+        { nama: "Danau Panggang", kode: "F73" },
+        { nama: "Haur Gading", kode: "F74" },
+        { nama: "Paminggir", kode: "F75" },
+        { nama: "Sungai Pandan", kode: "F76" },
+        { nama: "Sungai Tabukan", kode: "F77" }
+    ],
+    "07": [  // Kabupaten Kotabaru
+        { nama: "Hampang", kode: "F78" },
+        { nama: "Kelumpang Barat", kode: "F79" },
+        { nama: "Kelumpang Hilir", kode: "F80" },
+        { nama: "Kelumpang Hulu", kode: "F81" },
+        { nama: "Kelumpang Selatan", kode: "F82" },
+        { nama: "Kelumpang Tengah", kode: "F83" },
+        { nama: "Kelumpang Utara", kode: "F84" },
+        { nama: "Pamukan Barat", kode: "F85" },
+        { nama: "Pamukan Selatan", kode: "F86" },
+        { nama: "Pamukan Utara", kode: "F87" },
+        { nama: "Pulau Laut Barat", kode: "F88" },
+        { nama: "Pulau Laut Kepulauan", kode: "F89" },
+        { nama: "Pulau Laut Selatan", kode: "F90" },
+        { nama: "Pulau Laut Tanjung Selayar", kode: "F91" },
+        { nama: "Pulau Laut Tengah", kode: "F92" },
+        { nama: "Pulau Laut Timur", kode: "F93" },
+        { nama: "Pulau Laut Utara", kode: "F94" },
+        { nama: "Pulau Sebuku", kode: "F95" },
+        { nama: "Pulau Sembilan", kode: "F96" },
+        { nama: "Sampanahan", kode: "F97" },
+        { nama: "Sungai Durian", kode: "F98" }
+    ],
+    "08": [  // Kabupaten Tabalong
+        { nama: "Banua Lawas", kode: "F99" },
+        { nama: "Bintang Ara", kode: "F100" },
+        { nama: "Haruai", kode: "F101" },
+        { nama: "Jaro", kode: "F102" },
+        { nama: "Kelua", kode: "F103" },
+        { nama: "Muara Harus", kode: "F104" },
+        { nama: "Murung Pudak", kode: "F105" },
+        { nama: "Pugaan", kode: "F106" },
+        { nama: "Tanjung", kode: "F107" },
+        { nama: "Tanta", kode: "F108" },
+        { nama: "Upau", kode: "F109" },
+        { nama: "Muara Uya", kode: "F110" }
+    ],
+    "09": [  // Kabupaten Tanah Bumbu
+        { nama: "Angsana", kode: "F111" },
+        { nama: "Batulicin", kode: "F112" },
+        { nama: "Karang Bintang", kode: "F113" },
+        { nama: "Kusan Hilir", kode: "F114" },
+        { nama: "Kusan Hulu", kode: "F115" },
+        { nama: "Kuranji", kode: "F116" },
+        { nama: "Mantewe", kode: "F117" },
+        { nama: "Satui", kode: "F118" },
+        { nama: "Simpang Empat", kode: "F119" },
+        { nama: "Sungai Loban", kode: "F120" }
+    ],
+    "10": [  // Kabupaten Tanah Laut
+        { nama: "Bajuin", kode: "F121" },
+        { nama: "Batu Ampar", kode: "F122" },
+        { nama: "Bati-Bati", kode: "F123" },
+        { nama: "Jorong", kode: "F124" },
+        { nama: "Kintap", kode: "F125" },
+        { nama: "Kurau", kode: "F126" },
+        { nama: "Panyipatan", kode: "F127" },
+        { nama: "Pelaihari", kode: "F128" },
+        { nama: "Takisung", kode: "F129" },
+        { nama: "Tambang Ulang", kode: "F130" },
+        { nama: "Bumi Makmur", kode: "F131" }
+    ],
+    "11": [  // Kabupaten Tapin
+        { nama: "Bakarangan", kode: "F132" },
+        { nama: "Bungur", kode: "F133" },
+        { nama: "Candi Laras Selatan", kode: "F134" },
+        { nama: "Candi Laras Utara", kode: "F135" },
+        { nama: "Hatungun", kode: "F136" },
+        { nama: "Lokpaikat", kode: "F137" },
+        { nama: "Piani", kode: "F138" },
+        { nama: "Salam Babaris", kode: "F139" },
+        { nama: "Tapin Selatan", kode: "F140" },
+        { nama: "Tapin Tengah", kode: "F141" },
+        { nama: "Tapin Utara", kode: "F142" },
+        { nama: "Binuang", kode: "F143" }
+    ],
+    "12": [  // Kota Banjarbaru
+        { nama: "Banjarbaru Selatan", kode: "F144" },
+        { nama: "Banjarbaru Utara", kode: "F145" },
+        { nama: "Cempaka", kode: "F146" },
+        { nama: "Landasan Ulin", kode: "F147" },
+        { nama: "Liang Anggang", kode: "F148" }
+    ],
+    "13": [  // Kota Banjarmasin
+        { nama: "Banjarmasin Barat", kode: "F149" },
+        { nama: "Banjarmasin Selatan", kode: "F150" },
+        { nama: "Banjarmasin Tengah", kode: "F151" },
+        { nama: "Banjarmasin Timur", kode: "F152" },
+        { nama: "Banjarmasin Utara", kode: "F153" }
+    ]
+};
+// Update functions remain unchanged
+function updateKecamatan() {
+    const kabupatenSelect = document.getElementById('kabupaten');
+    const kecamatanSelect = document.getElementById('kecamatan');
+    
+    kecamatanSelect.innerHTML = '<option value="">Pilih Kecamatan</option>';
+    
+    const selectedKabupaten = kabupatenSelect.value;
+    const kecamatans = kecamatanData[selectedKabupaten];
+    
+    if (kecamatans) {
+        kecamatans.forEach(kecamatan => {
+            const option = document.createElement('option');
+            option.value = kecamatan.kode;
+            option.textContent = kecamatan.nama;
+            kecamatanSelect.appendChild(option);
+        });
+    }
+}
+
+function updateKodeWilayah() {
+    const kecamatanSelect = document.getElementById('kecamatan');
+    
+    const selectedKecamatan = kecamatanSelect.value;
+    
+    if (selectedKecamatan) {
+        const kodeWilayah = selectedKecamatan;
+        document.getElementById('kode-wilayah').innerText = kodeWilayah;
+        document.getElementById('contoh-output').innerText = `{${kodeWilayah}RAHMAD}`;
+        document.getElementById('output').style.display = 'block';
+    } else {
+        document.getElementById('output').style.display = 'none';
+    }
+}
