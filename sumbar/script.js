7755bf95993f23cba694f93cf61f6eef8c5aede7
Bemat <116222049+yuusuke1101@users.noreply.github.com>
2024-10-23 15:10:49 +0700

    Add files via upload

---
+++ sumbar/script.js
@@ -0,0 +1,257 @@
+const kecamatanData = {
+    "01": [  // Kabupaten Agam
+        { nama: "Ampek Angkek", kode: "I01" },
+        { nama: "Ampek Nagari", kode: "I02" },
+        { nama: "Banuhampu", kode: "I03" },
+        { nama: "Baso", kode: "I04" },
+        { nama: "Candung", kode: "I05" },
+        { nama: "Kamang Magek", kode: "I06" },
+        { nama: "Lubuk Basung", kode: "I07" },
+        { nama: "Malalak", kode: "I08" },
+        { nama: "Matur", kode: "I09" },
+        { nama: "Palembayan", kode: "I10" },
+        { nama: "Palupuh", kode: "I11" },
+        { nama: "IV Koto", kode: "I12" },
+        { nama: "Sungaipua", kode: "I13" },
+        { nama: "Tanjung Mutiara", kode: "I14" },
+        { nama: "Tanjung Raya", kode: "I15" },
+        { nama: "Tilatang Kamang", kode: "I16" },
+    ],
+    "02": [  // Kabupaten Dharmasraya
+        { nama: "Asam Jujuhan", kode: "I17" },
+        { nama: "Koto Besar", kode: "I18" },
+        { nama: "Koto Baru", kode: "I19" },
+        { nama: "Pulau Punjung", kode: "I20" },
+        { nama: "Sembilan Koto", kode: "I21" },
+        { nama: "Sungai Rumbai", kode: "I22" },
+        { nama: "Timpeh", kode: "I23" },
+        { nama: "Padang Laweh", kode: "I24" },
+        { nama: "Tiumang", kode: "I25" },
+        { nama: "Koto Salak", kode: "I26" },
+        { nama: "Koto Tangah Darussalam", kode: "I27" },
+    ],
+	    "03": [  // Kabupaten Kepulauan Mentawai
+        { nama: "Pagai Selatan", kode: "I28" },
+        { nama: "Pagai Utara", kode: "I29" },
+        { nama: "Sikakap", kode: "I30" },
+        { nama: "Siberut Barat", kode: "I31" },
+        { nama: "Siberut Barat Daya", kode: "I32" },
+        { nama: "Siberut Selatan", kode: "I33" },
+        { nama: "Siberut Tengah", kode: "I34" },
+        { nama: "Siberut Utara", kode: "I35" },
+        { nama: "Sipora Selatan", kode: "I36" },
+        { nama: "Sipora Utara", kode: "I37" }
+    ],
+    "04": [  // Kabupaten Lima Puluh Kota
+        { nama: "Akabiluru", kode: "I38" },
+        { nama: "Bukik Barisan", kode: "I39" },
+        { nama: "Guguak", kode: "I40" },
+        { nama: "Gunuang Omeh", kode: "I41" },
+        { nama: "Harau", kode: "I42" },
+        { nama: "Kapur IX", kode: "I43" },
+        { nama: "Lareh Sago Halaban", kode: "I44" },
+        { nama: "Luak", kode: "I45" },
+        { nama: "Mungka", kode: "I46" },
+        { nama: "Pangkalan Koto Baru", kode: "I47" },
+        { nama: "Payakumbuh", kode: "I48" },
+        { nama: "Situjuah Limo Nagari", kode: "I49" },
+        { nama: "Suliki", kode: "I50" }
+    ],
+    "05": [  // Kabupaten Padang Pariaman
+        { nama: "2 x 11 Enam Lingkung", kode: "I51" },
+        { nama: "Batang Anai", kode: "I52" },
+        { nama: "Batang Gasan", kode: "I53" },
+        { nama: "Enam Lingkung", kode: "I54" },
+        { nama: "IV Koto Aur Malintang", kode: "I55" },
+        { nama: "Lubuk Alung", kode: "I56" },
+        { nama: "Nan Sabaris", kode: "I57" },
+        { nama: "Padang Sago", kode: "I58" },
+        { nama: "Patamuan", kode: "I59" },
+        { nama: "Sintuk Toboh Gadang", kode: "I60" },
+        { nama: "Sungai Geringging", kode: "I61" },
+        { nama: "Sungai Limau", kode: "I62" },
+        { nama: "Ulakan Tapakis", kode: "I63" },
+        { nama: "V Koto Kampung Dalam", kode: "I64" },
+        { nama: "V Koto Timur", kode: "I65" },
+        { nama: "VII Koto Sungai Sarik", kode: "I66" },
+        { nama: "Pariaman", kode: "I67" }
+    ],
+    "06": [  // Kabupaten Pasaman
+        { nama: "Bonjol", kode: "I68" },
+        { nama: "Dua Koto", kode: "I69" },
+        { nama: "Lubuk Sikaping", kode: "I70" },
+        { nama: "Mapat Tunggul", kode: "I71" },
+        { nama: "Mapat Tunggul Selatan", kode: "I72" },
+        { nama: "Padang Gelugur", kode: "I73" },
+        { nama: "Panti", kode: "I74" },
+        { nama: "Rao", kode: "I75" },
+        { nama: "Rao Selatan", kode: "I76" },
+        { nama: "Rao Utara", kode: "I77" },
+        { nama: "Simpati", kode: "I78" },
+        { nama: "Tigo Nagari", kode: "I79" }
+    ],
+    "07": [  // Kabupaten Pasaman Barat
+        { nama: "Gunung Tuleh", kode: "I80" },
+        { nama: "Kinali", kode: "I81" },
+        { nama: "Lembah Melintang", kode: "I82" },
+        { nama: "Luhak Nan Duo", kode: "I83" },
+        { nama: "Pasaman", kode: "I84" },
+        { nama: "Ranah Batahan", kode: "I85" },
+        { nama: "Sasak Ranah Pasisie", kode: "I86" },
+        { nama: "Sei Beremas", kode: "I87" },
+        { nama: "Sungai Aur", kode: "I88" },
+        { nama: "Talamau", kode: "I89" },
+        { nama: "Sungai Beremas", kode: "I90" }
+    ],
+    "08": [  // Kabupaten Pesisir Selatan
+        { nama: "Airpura", kode: "I91" },
+        { nama: "Basa Ampek Balai Tapan", kode: "I92" },
+        { nama: "Batangkapas", kode: "I93" },
+        { nama: "Bayang", kode: "I94" },
+        { nama: "IV Jurai", kode: "I95" },
+        { nama: "IV Nagari Bayang Utara", kode: "I96" },
+        { nama: "Koto XI Tarusan", kode: "I97" },
+        { nama: "Lengayang", kode: "I98" },
+        { nama: "Linggo Sari Baganti", kode: "I99" },
+        { nama: "Lunang", kode: "I100" },
+        { nama: "Pancung Soal", kode: "I101" },
+        { nama: "Ranah Ampek Hulu Tapan", kode: "I102" },
+        { nama: "Ranah Pesisir", kode: "I103" },
+        { nama: "Silaut", kode: "I104" },
+        { nama: "Sutera", kode: "I105" }
+    ],
+		"09": [  // Kabupaten Sijunjung
+		{ nama: "IV Nagari", kode: "I106" },
+		{ nama: "Kamang Baru", kode: "I107" },
+		{ nama: "Koto VII", kode: "I108" },
+		{ nama: "Kupitan", kode: "I109" },
+		{ nama: "Lubuk Tarok", kode: "I110" },
+		{ nama: "Sijunjung", kode: "I111" },
+		{ nama: "Tanjung Gadang", kode: "I112" },
+		{ nama: "Sumpur Kudus", kode: "I113" }
+	],
+	"10": [  // Kabupaten Solok
+		{ nama: "Bukit Sundi", kode: "I114" },
+		{ nama: "Danau Kembar", kode: "I115" },
+		{ nama: "Gunung Talang", kode: "I116" },
+		{ nama: "Hiliran Gumanti", kode: "I117" },
+		{ nama: "IX Koto Sungai Lasi", kode: "I118" },
+		{ nama: "Kubung", kode: "I119" },
+		{ nama: "Lembah Gumanti", kode: "I120" },
+		{ nama: "Lembang Jaya", kode: "I121" },
+		{ nama: "Pantai Cermin", kode: "I122" },
+		{ nama: "Payung Sekaki", kode: "I123" },
+		{ nama: "Tigo Lurah", kode: "I124" },
+		{ nama: "X Koto Diatas", kode: "I125" },
+		{ nama: "X Koto Singkarak", kode: "I126" },
+		{ nama: "Junjung Sirih", kode: "I127" }
+	],
+	"11": [  // Kabupaten Solok Selatan
+		{ nama: "Koto Parik Gadang Diateh", kode: "I128" },
+		{ nama: "Pauh Duo", kode: "I129" },
+		{ nama: "Sangir", kode: "I130" },
+		{ nama: "Sangir Balai Janggo", kode: "I131" },
+		{ nama: "Sangir Batang Hari", kode: "I132" },
+		{ nama: "Sangir Jujuan", kode: "I133" },
+		{ nama: "Sungai Pagu", kode: "I134" }
+	],
+	"12": [  // Kabupaten Tanah Datar
+		{ nama: "Batipuh", kode: "I135" },
+		{ nama: "Batipuh Selatan", kode: "I136" },
+		{ nama: "Lintau Buo", kode: "I137" },
+		{ nama: "Lintau Buo Utara", kode: "I138" },
+		{ nama: "Padang Ganting", kode: "I139" },
+		{ nama: "Pariangan", kode: "I140" },
+		{ nama: "Rambatan", kode: "I141" },
+		{ nama: "Salimpaung", kode: "I142" },
+		{ nama: "Sepuluh Koto", kode: "I143" },
+		{ nama: "Sungai Tarab", kode: "I144" },
+		{ nama: "Sungayang", kode: "I145" },
+		{ nama: "Tanjung Emas", kode: "I146" },
+		{ nama: "Tanjung Baru", kode: "I147" },
+		{ nama: "Lima Kaum", kode: "I148" }
+	],
+	"13": [  // Kota Bukittinggi
+		{ nama: "Aur Birugo Tigo Baleh", kode: "I149" },
+		{ nama: "Guguk Panjang", kode: "I150" },
+		{ nama: "Mandiangin Koto Selayan", kode: "I151" }
+	],
+	"14": [  // Kota Padang
+		{ nama: "Bungus Teluk Kabung", kode: "I152" },
+		{ nama: "Koto Tangah", kode: "I153" },
+		{ nama: "Kuranji", kode: "I154" },
+		{ nama: "Lubuk Begalung", kode: "I155" },
+		{ nama: "Lubuk Kilangan", kode: "I156" },
+		{ nama: "Nanggalo", kode: "I157" },
+		{ nama: "Padang Barat", kode: "I158" },
+		{ nama: "Padang Selatan", kode: "I159" },
+		{ nama: "Padang Timur", kode: "I160" },
+		{ nama: "Padang Utara", kode: "I161" },
+		{ nama: "Pauh", kode: "I162" }
+	],
+	"15": [  // Kota Padangpanjang
+		{ nama: "Padang Panjang Barat", kode: "I163" },
+		{ nama: "Padang Panjang Timur", kode: "I164" }
+	],
+	"16": [  // Kota Pariaman
+		{ nama: "Pariaman Selatan", kode: "I165" },
+		{ nama: "Pariaman Tengah", kode: "I166" },
+		{ nama: "Pariaman Timur", kode: "I167" },
+		{ nama: "Pariaman Utara", kode: "I168" }
+	],
+	"17": [  // Kota Payakumbuh
+		{ nama: "Payakumbuh Barat", kode: "I169" },
+		{ nama: "Payakumbuh Selatan", kode: "I170" },
+		{ nama: "Payakumbuh Timur", kode: "I171" },
+		{ nama: "Payakumbuh Utara", kode: "I172" },
+		{ nama: "Lamposi Tigo Nagari", kode: "I173" }
+	],
+	"18": [  // Kota Sawahlunto
+		{ nama: "Barangin", kode: "I174" },
+		{ nama: "Lembah Segar", kode: "I175" },
+		{ nama: "Silungkang", kode: "I176" },
+		{ nama: "Talawi", kode: "I177" }
+	],
+	"19": [  // Kota Solok
+		{ nama: "Lubuk Sikarah", kode: "I178" },
+		{ nama: "Tanjung Harapan", kode: "I179" }
+	]
+
+
+};
+
+
+// Update functions remain unchanged
+function updateKecamatan() {
+    const kabupatenSelect = document.getElementById('kabupaten');
+    const kecamatanSelect = document.getElementById('kecamatan');
+    
+    kecamatanSelect.innerHTML = '<option value="">Pilih Kecamatan</option>';
+    
+    const selectedKabupaten = kabupatenSelect.value;
+    const kecamatans = kecamatanData[selectedKabupaten];
+    
+    if (kecamatans) {
+        kecamatans.forEach(kecamatan => {
+            const option = document.createElement('option');
+            option.value = kecamatan.kode;
+            option.textContent = kecamatan.nama;
+            kecamatanSelect.appendChild(option);
+        });
+    }
+}
+
+function updateKodeWilayah() {
+    const kecamatanSelect = document.getElementById('kecamatan');
+    
+    const selectedKecamatan = kecamatanSelect.value;
+    
+    if (selectedKecamatan) {
+        const kodeWilayah = selectedKecamatan;
+        document.getElementById('kode-wilayah').innerText = kodeWilayah;
+        document.getElementById('contoh-output').innerText = `{${kodeWilayah}RAHMAD}`;
+        document.getElementById('output').style.display = 'block';
+    } else {
+        document.getElementById('output').style.display = 'none';
+    }
+}
